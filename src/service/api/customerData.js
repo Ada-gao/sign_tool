e5a8673933d671e89e76dd37f56d87fe96b36343
liulong <1046085296@qq.com>
2018-05-23 15:25:11 +0800

    增加省市区联动数据和客户列表模拟数据

---
+++ src/service/api/customerData.js
@@ -0,0 +1,66 @@
+/**
+ * Created by liulong on 2018/5/22.
+ */
+let customerData = [
+  {
+    'name': '张三',
+    'mobile': '13852793508',
+    'id_type': '0',
+    'id_no': '413026199011116723',
+    'wechat': 'lcy1771705',
+    'email': 'spring@shunyun365.com',
+    'nationality': '中国',
+    'user_id': 37,
+    'city': '上海',
+    'client_class': 0,
+    'certification_status': 0,
+    'client_type': 0,
+    'client_id': 0
+  },
+  {
+    'name': '李四',
+    'mobile': '13852793508',
+    'id_type': '0',
+    'id_no': '413026199011116723',
+    'wechat': 'lcy1771705',
+    'email': 'spring@shunyun365.com',
+    'nationality': '中国',
+    'user_id': 37,
+    'city': '上海',
+    'client_class': 0,
+    'certification_status': 1,
+    'client_type': 1,
+    'client_id': 1
+  },
+  {
+    'name': '王五',
+    'mobile': '13852793508',
+    'id_type': '0',
+    'id_no': '413026199011116723',
+    'wechat': 'lcy1771705',
+    'email': 'spring@shunyun365.com',
+    'nationality': '中国',
+    'user_id': 37,
+    'city': '上海',
+    'client_class': 1,
+    'certification_status': 0,
+    'client_type': 0,
+    'client_id': 2
+  },
+  {
+    'name': '赵六',
+    'mobile': '13852793508',
+    'id_type': '0',
+    'id_no': '413026199011116723',
+    'wechat': 'lcy1771705',
+    'email': 'spring@shunyun365.com',
+    'nationality': '中国',
+    'user_id': 37,
+    'city': '上海',
+    'client_class': 1,
+    'certification_status': 1,
+    'client_type': 1,
+    'client_id': 3
+  }
+]
+export {customerData}
