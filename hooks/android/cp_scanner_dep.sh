2b0c3ea95b846a14e98575358c37bc30a25afbd8
John Zhuang <zhuangyinping@shuyun365.com>
2018-11-07 22:53:14 +0800

    change to staging env

--- hooks/android/cp_scanner_dep.sh
+++ hooks/android/cp_scanner_dep.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-cp plugins/shuyunqrcodescanner/src/android/gizscanqrcode-release.aar platforms/android/app/libs/gizscanqrcode.aar 2> /dev/null 
+cp plugins/shuyunqrcodescanner/src/android/gizscanqrcode-release.aar platforms/android/app/libs/gizscanqrcode.aar 2> /dev/null
 
